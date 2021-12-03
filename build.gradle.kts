3cf46fe72bdd62c2ef682b3645e3dc26c047af91
Dmitrii Rashchenko <dimitree54@gmail.com>
2021-12-03 08:13:34 +0200

    version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 }
 
 dependencies {
-	implementation("we.rashchenko:chnn-library:v0.1.3")
+	implementation("we.rashchenko:chnn-library:v0.1.4")
 	testImplementation("org.openjdk.jol:jol-core:0.16")
 }
 
